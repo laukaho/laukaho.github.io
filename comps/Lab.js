f77276723079b3e4a524925e527f82226b4e4db5
laukaho <kaholau.dev@gmail.com>
2021-05-26 00:21:58 +0800

    Use Link in Lab

--- comps/Lab.js
+++ comps/Lab.js
@@ -6,18 +6,20 @@ import styles from "../styles/Lab.module.css"
 export default function Lab(props) {
     console.log(props);
     return(
-        <a href={props.path} className={styles.lab}>
-            <span className={styles.image}  style={{backgroundImage: "url(/images/test.png)"}}>
-            </span>
-            <span className={styles.detail} >
-            <span className={styles.title} >
-                {props.post.data.title}<span className={styles.date} >{props.post.data.date}</span>
-            </span>
-            <span className={styles.description}>
-                {props.post.data.description}
-            </span>
-            </span>
-        </a>
+        <Link href={props.path} passHref>
+            <a className={styles.lab}>
+                <span className={styles.image}  style={{backgroundImage: "url(/images/test.png)"}}>
+                </span>
+                <span className={styles.detail} >
+                <span className={styles.title} >
+                    {props.post.data.title}<span className={styles.date} >{props.post.data.date}</span>
+                </span>
+                <span className={styles.description}>
+                    {props.post.data.description}
+                </span>
+                </span>
+            </a>
+        </Link>
     )
 }
 
