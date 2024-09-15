2bd29d629589643a20820a6e50a8d7cfbc2034c4
automation <kaholau.dev@gmail.com>
2024-09-16 01:59:46 +0800

    fix: update gh action

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,20 @@
 npm run clear
 npm run build
 
-git stash
+
 git pull origin gh-pages --rebase
-git stash pop
 git checkout gh-pages
 
 cp -r ./build/* .
 git add .
 # Set up timestamp for commit message
-timestamp=$(date +"%Y%m%d-%H%M%S")
 
 # Configure Git identity
 git config --local user.email "kaholau.dev@gmail.com"
-git config --local user.name "automation"
+git config --local user.name "laukaho"
 
 # Commit and push changes
+timestamp=$(date +"%Y%m%d-%H%M%S")
 git commit -m "Publish page $timestamp"
 git push origin gh-pages
 git checkout master
